2562a364684ab2e8743013ee1d97a8ee4eecb3d1
Victor Lau <lau.victor.w@gmail.com>
2021-12-28 20:06:28 -0800

    refactor: change the naming to be alphabetical
    Change it to be alphabetical to match it easier.

--- worker/models/client_decentralization.go
+++ worker/models/client_decentralization.go
@@ -1,21 +1,18 @@
 package models
 
-import (
-	"time"
-)
-
 type ClientDecentralization struct {
+	ASN           float64 //Autonomous System Number
+	Blockchain    string  `validate:"required"`
+	City          string
 	Country       string `validate:"required"`
 	Client        string `validate:"required"`
-	Region        string `validate:"required"`
-	Timezone      string
-	City          string
-	Blockchain    string    `validate:"required"`
-	Timestamp     time.Time `validate:"required"`
-	ClientVersion string    `validate:"required"`
-	Organization  string    // Organization who owns this
+	ClientVersion string `validate:"required"`
 	IPAddress     string
 	Longitude     float64
 	Latitude      float64
-	ASN           float64 //Autonomous System Number
+	Organization  string // Organization who manages this
+	Region        string `validate:"required"`
+	Validator     bool   //Required for PoS system. Defaults to false for PoW Systems
+	Timestamp     string `validate:"required"` // Format must be time.RFC3339
+	Timezone      string
 }
--- worker/models/node_decentralization.go
+++ worker/models/node_decentralization.go
@@ -1,11 +1,15 @@
 package models
 
+import "time"
+
 type NodeDecentralization struct {
-	Country   string
-	Host      string
-	Isp       string
-	ASN       float64
-	Validator bool //Required for PoS system. Defaults to false for PoW
-	Longitude float64
-	Latitude  float64
+	ASN        float64 //Grouping of IP addresses managed by 1 service
+	Blockchain string  `validate:"required"`
+	City       string
+	Country    string
+	IPAddress  string
+	Longitude  float64
+	Latitude   float64
+	Timestamp  time.Time
+	Validator  bool //Required for PoS system. Defaults to false for PoW
 }
